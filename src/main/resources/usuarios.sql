2d32c8bf03cc9014a03dd0cbf5a44c05244291fe
MiguelMayoM <55299842+MiguelMayoM@users.noreply.github.com>
2019-12-18 20:31:00 +0100

    Update usuarios.sql

--- src/main/resources/usuarios.sql
+++ src/main/resources/usuarios.sql
@@ -1,11 +1,11 @@
--- DROP DATABASE IF EXISTS `usuarios`;
--- CREATE DATABASE `usuarios` IF NOT EXISTS;
+DROP DATABASE IF EXISTS `usuarios`;
+CREATE DATABASE `usuarios` IF NOT EXISTS;
 USE `usuarios`;
 
 DROP TABLE IF EXISTS `authorities`;
 CREATE TABLE `authorities` (`username` varchar(50) NOT NULL, `authority` varchar(50) NOT NULL, UNIQUE KEY `ix_auth_username` (`username`,`authority`), CONSTRAINT `fk_authorities_users` FOREIGN KEY (`username`) REFERENCES `users` (`username`)) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `users`
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (`username` varchar(50) NOT NULL, `password` varchar(50) NOT NULL, `enabled` tinyint(1) NOT NULL, PRIMARY KEY (`username`)) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
